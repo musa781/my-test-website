95741ad3340a5f31fb8e56f38e7aa9c0e53d7b9c
Usama <sharifusama351@gmail.com>
2026-04-27 13:02:43 +0500

    feat: updated main Roman to English

--- app/about/page.js
+++ app/about/page.js
@@ -1,8 +1,8 @@
 export default function AboutPage() {
   return (
     <div className="p-20">
-      <h1 className="text-3xl font-bold">Mere Baare Mein</h1>
-      <p>Main aik web developer hoon aur ye mera about page hai.</p>
+      <h1 className="text-3xl font-bold">About Page</h1>
+      <p>I'm a web developer and this is my about page.</p>
     </div>
   );
 }
--- app/page.js
+++ app/page.js
@@ -4,15 +4,15 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div className=" p-20">
-      <h1 className="text-4xl font-bold">Salam! Ye Meri Pehli Next.js Site Hai 🚀</h1>
-      <p className="mt-4 text-gray-600">Main abhi Next.js seekh raha hoon.</p>
+      <h1 className="text-4xl font-bold">Salam! This is my first nextjs website 🚀</h1>
+      <p className="mt-4 text-gray-600">I'm a web developer and learning Next js.</p>
       
       {/* Ye hamara naya button hai */}
       <div className='flex gap-6'>
       <div className="mt-8">
 
         <Link href="/about" className="bg-blue-600 text-white px-5 py-2 rounded-md hover:bg-blue-700">
-          Mere Baare Mein Janiye ➡️
+          About Me ➡️
         </Link>
       </div>
       <div className="mt-8">
